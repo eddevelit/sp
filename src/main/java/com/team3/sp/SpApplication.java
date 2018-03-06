0663979db1f9797816f07db32aadcfdc6c75a091
Javier Ramirez <cabestro_jj@hotmail.com>
2018-03-06 15:01:07 -0600

    Añadiendo modelos

--- src/main/java/com/team3/sp/SpApplication.java
+++ src/main/java/com/team3/sp/SpApplication.java
@@ -1,12 +1,44 @@
 package com.team3.sp;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team3.sp.model.Cliente;
 
 @SpringBootApplication
 public class SpApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SpApplication.class, args);
+		escribirJSON();//Crear archivo JASON
 	}
+	////////Prueba JSON--- Objeto a JSON
+	public static void escribirJSON() throws IOException {
+		Cliente cliente = new Cliente();
+		cliente.setId(1);
+		cliente.setNombre("Juan Perez");
+		cliente.setDireccion("Av. Tehuantepec");
+		cliente.setCorreo("juanito.banana@yahoo.com");
+		cliente.setTelefono("55463728");
+		cliente.setSaldo(10000.00);
+		System.out.println(cliente);
+		
+		/*Crea archivo JSON
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cliente);
+		
+		mapper.writerWithDefaultPrettyPrinter().writeValue(new File("D:\\cliente.json"), cliente);
+		System.out.println("SALIDA JSON: \n" + json);*///Crea archivo JSON
+		ObjectMapper mapper = new ObjectMapper();
+
+		Cliente client = mapper.readValue(new File("D:\\cliente.json"), Cliente.class);
+
+		System.out.println("OBJETO CLIENTE: \n" + client);
+	}
+	////////////////////
 }
---
+++ src/main/java/com/team3/sp/model/Cuenta.java
@@ -0,0 +1,51 @@
+package com.team3.sp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cuenta3")
+public class Cuenta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private double Saldo;
+	
+	@Column
+	private String Ejecutivo;
+	
+	@Column
+	private int id_cliente;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getSaldo() {
+		return Saldo;
+	}
+	public void setSaldo(double saldo) {
+		Saldo = saldo;
+	}
+	public String getEjecutivo() {
+		return Ejecutivo;
+	}
+	public void setEjecutivo(String ejecutivo) {
+		Ejecutivo = ejecutivo;
+	}
+	public int getId_cliente() {
+		return id_cliente;
+	}
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+}
