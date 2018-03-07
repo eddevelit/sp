29eb5fe1191820640596e0c109c693a845db9434
eddevelit <edd.develit@gmail.com>
2018-03-06 21:35:15 -0600

    Se modifica IndexController iniciando un objeto model y realizando una instancia de Cliente

--- src/main/java/com/team3/sp/controller/IndexController.java
+++ src/main/java/com/team3/sp/controller/IndexController.java
@@ -3,15 +3,17 @@ package com.team3.sp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.ui.Model;
 import com.team3.sp.model.Cliente;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping("/")
-	public String index()
+	public String index(Model model)
 	{
+		//Creamos objeto de cliente
+		model.addAttribute("cliente", new Cliente());
 	return "index";	
 	}
 	
